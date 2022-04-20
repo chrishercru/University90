5b60599501695b5f462b6fed9a1f16c087dccc97
chrishercru <74775365+chrishercru@users.noreply.github.com>
2022-04-19 20:19:34 -0500

    19 abril 2022

--- University.Web/Controllers/DashboardController.cs
+++ University.Web/Controllers/DashboardController.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Mvc;
+using University.BL.DTOs;
 
 namespace University.Web.Controllers
 {
@@ -10,6 +13,22 @@ namespace University.Web.Controllers
     {
         // GET: Dashboard
         public ActionResult Donut()
+        {
+            return View();
+        }
+        public async Task<ActionResult> DonutJson()
+        {
+            var data = new List<DonutExampleDTO>();
+            data.Add(new DonutExampleDTO { Value = 70, Label = "Java" });
+            data.Add(new DonutExampleDTO { Value = 30, Label = "Angular" });
+
+            var dataJson = JsonConvert.SerializeObject(data);
+
+            return Json(dataJson, JsonRequestBehavior.AllowGet);
+        }
+
+       
+        public ActionResult Bar()
         {
             return View();
         }
