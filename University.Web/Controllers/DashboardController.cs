b13bfea052fb6e06989918280d9b96ec055b2c8f
chrishercru <74775365+chrishercru@users.noreply.github.com>
2022-04-19 19:19:54 -0500

    Actualizacion1

---
+++ University.Web/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace University.Web.Controllers
+{
+    public class DashboardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Donut()
+        {
+            return View();
+        }
+    }
+}
